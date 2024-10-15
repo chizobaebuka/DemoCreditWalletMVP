3025efa4e40bb3a61e52e4c85b5c5c3741613754
martins <chizobaebuka1@gmail.com>
2024-10-15 14:26:48 +0100

    Add login functionality and user routes

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from 'express';
-import { signupSchema } from '../utils/validators';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { loginSchema, signupSchema } from '../utils/validators';
 import connection from '../db/database';
 
 export const createUser = async (req: Request, res: Response): Promise<void> => {
-    console.log('Incoming request body:', req.body); // Log the request body
-
     try {
         const validatedData = signupSchema.parse(req.body);
         const { name, email, password } = validatedData;
@@ -15,8 +15,10 @@ export const createUser = async (req: Request, res: Response): Promise<void> => 
             return; // Exit the function after sending a response
         }
 
+        const hashedPassword = await bcrypt.hash(password, 10);
+
         const [newUser] = await connection('users')
-            .insert({ name, email, password })
+            .insert({ name, email, password: hashedPassword })
             .returning('*');
 
         res.status(201).json(newUser); // Send the new user response
@@ -25,3 +27,59 @@ export const createUser = async (req: Request, res: Response): Promise<void> => 
         res.status(500).json({ error: 'Failed to create user' });
     }
 };
+
+export const loginUser = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const validatedData = loginSchema.parse(req.body);
+        const { email, password } = validatedData;
+
+        const user = await connection('users').where({ email }).first();
+        if (!user) {
+            res.status(401).json({ error: 'Invalid credentials' });
+            return;
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            res.status(401).json({ error: 'Invalid credentials' });
+            return;
+        }
+
+        // generate jwt token and send it to the user
+        const token = jwt.sign(
+            { userId: user.id },
+            process.env.JWT_SECRET_KEY as string,
+            { expiresIn: process.env.JWT_EXPIRY as string }
+        );
+        
+        res.status(200).json({ message: 'Login successful', data: user, token });
+    } catch (error) {
+        console.error('Error logging in user:', error);
+        res.status(500).json({ error: 'Failed to log in' });
+    }
+};
+
+export const getAllUsers = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const users = await connection('users').select('*');
+        res.status(200).json(users);
+    } catch (error) {
+        console.error('Error getting users:', error);
+        res.status(500).json({ error: 'Failed to get users' });
+    }
+}
+
+export const getUserById = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { id } = req.params;
+        const user = await connection('users').where({ id }).first();
+        if (!user) {
+            res.status(404).json({ error: 'User not found' });
+            return;
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        console.error('Error getting user:', error);
+        res.status(500).json({ error: 'Failed to get user' });
+    }
+}
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,11 +1,14 @@
 // src/routes/userRoutes.ts
 
 import { Router } from 'express';
-import { createUser } from '../controllers/user.controller';
+import { createUser, getAllUsers, getUserById, loginUser } from '../controllers/user.controller';
 
 const userRouter = Router();
 
 // Route for creating a new user
 userRouter.post('/signup', createUser);
+userRouter.post('/login', loginUser);
+userRouter.get('/', getAllUsers);
+userRouter.get('/:id', getUserById);
 
 export default userRouter;
